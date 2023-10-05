f6d2bc327c7756f362176d133e330e50524702ab
Sky1433 <Samaanawaf@gmail.com>
2023-10-05 12:00:24 +0300

    Initial Commit

---
+++ test3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test3
+//
+//  Created by Samaa on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test3/test3App.swift
@@ -0,0 +1,17 @@
+//
+//  test3App.swift
+//  test3
+//
+//  Created by Samaa on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
