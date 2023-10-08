c7552f3c0252b90df223f79bcbb873fc3915e151
Sky1433 <Samaanawaf@gmail.com>
2023-10-08 10:55:43 +0300

    profile

---
+++ test3/profile.swift
@@ -0,0 +1,18 @@
+//
+//  profile.swift
+//  test3
+//
+//  Created by Samaa on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profile()
+}
